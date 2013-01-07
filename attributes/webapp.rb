6e278148a3175f7d6d1997139b4fc6c9a6002ff7
Nick Whitt <nick.whitt@gmail.com>
2013-01-06 23:49:46 -0500

    Creating a webapp recipe. Utilizes web_app resource from apache2 cookbook.

---
+++ attributes/webapp.rb
@@ -0,0 +1,26 @@
+#
+# Author:: Nicholas Whitt (<nick.whitt@gmail.com>)
+# Cookbook Name:: devtools
+# Attribute:: webapp
+#
+# Copyright 2012, Nicholas Whitt.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default[:webapp][:proj] = "myproj"
+default[:webapp][:docroot] = "/var/www/#{node[:webapp][:proj]}"
+default[:webapp][:server_name] = node[:fqdn]
+default[:webapp][:options] = ["All"]
+default[:webapp][:allow_override] = ["All"]
+default[:webapp][:directory_index] = []
---
+++ recipes/webapp.rb
@@ -0,0 +1,31 @@
+#
+# Author:: Nicholas Whitt (<nick.whitt@gmail.com>)
+# Cookbook Name:: devtools
+# Recipe:: webapp
+#
+# Copyright 2012, Nicholas Whitt.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "apache2"
+
+web_app node[:webapp][:proj] do
+  docroot node[:webapp][:docroot]
+  template "webapp.conf.erb"
+  server_name node[:webapp][:server_name]
+  server_aliases [node[:hostname], node[:webapp][:proj]]
+  options node[:webapp][:options]
+  allow_override node[:webapp][:allow_override]
+  directory_index node[:webapp][:directory_index]
+end
